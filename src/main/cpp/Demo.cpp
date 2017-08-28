49f6d926107b0d0cf91f692e7583db4e710c20c3
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-27 23:37:22 -0500

    CSC232 - Demonstration of using as a typedef in C++11.

--- src/main/cpp/Demo.cpp
+++ src/main/cpp/Demo.cpp
@@ -10,6 +10,9 @@
 
 #include <iostream>
 
+using length_unit = double;
+using volumne_unit = double;
+
 /**
  * @brief Entry point for this application.
  *
@@ -18,12 +21,12 @@
 int main() {
     // Compute the volume of a sphere of a given radius
     const double PI{3.14159};
-    double radius;
+    length_unit radius;
 
     std::cout << "Enter the radius of the sphere: ";
     std::cin >> radius;
 
-    double volume{4 * PI * radius * radius * radius / 3};
+    volumne_unit volume{4 * PI * radius * radius * radius / 3};
     std::cout << "The volume of a sphere of radius "
               << radius << " inches is " << volume
               << " cubic inches.\n";
