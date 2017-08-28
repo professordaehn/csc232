8c8abd210eea403e2b27b1b43b6fa79e7983feed
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-27 22:51:23 -0500

    CSC232-APPA - Initial import of base C++ programs.

---
+++ src/main/cpp/Main.cpp
@@ -0,0 +1,20 @@
+/**
+ * CSC232 - Data Structures with C++
+ * Missouri State University, Fall 2017
+ *
+ * @file   Main.cpp
+ * @author Jim Daehn <jdaehn@missouristate.edu>
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * @brief Entry point for this application.
+ *
+ * @return 0 is returned upon successful execution.
+ */
+int main() {
+    std::cout << "Hello from Main.cpp!" << std::endl;
+    return EXIT_SUCCESS;
+} // end program
