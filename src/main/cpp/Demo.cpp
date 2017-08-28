c0b5186ddc7ee5615c87e416c0a7d6e86f7b6271
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-28 00:31:06 -0500

    CSC232 - Correct spelling error in using type, change PRODUCTION defintion and demonstrate stream manipulators.

--- src/main/cpp/Demo.cpp
+++ src/main/cpp/Demo.cpp
@@ -10,13 +10,14 @@
 
 #define FALSE 0
 #define TRUE 1
-#define PRODUCTION FALSE
+#define PRODUCTION TRUE
 
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 
 using length_unit = double;
-using volumne_unit = double;
+using volume_unit = double;
 
 /**
  * @brief Entry point for this application.
@@ -46,9 +47,14 @@ int main() {
     // Similarly here, instead of using typedef we use the
     // using statement from above to declare volume_unit as
     // a "new" type that is equivalent to double
-    volumne_unit volume{4 * PI * radius * radius * radius / 3};
+    volume_unit volume{4 * PI * radius * radius * radius / 3};
+    std::cout << std::showpoint;
     std::cout << "The volume of a sphere of radius "
-              << radius << " inches is " << volume
+              // Notice that precision has to do with the number of
+              // significant digits, and not the number of decimals
+              // after the decimal point.
+              << std::setprecision(2) << radius << " inches is "
+              << std::setprecision(4) << volume
               << " cubic inches.\n";
 
     return EXIT_SUCCESS;
