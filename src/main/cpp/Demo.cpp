8da55da3bd862df16e5e4f88969b89f387e105ea
Mr. Daehn <jdaehn@missouristate.edu>
2017-08-27 23:32:50 -0500

    CSC232 - Use standard initializers.

--- src/main/cpp/Demo.cpp
+++ src/main/cpp/Demo.cpp
@@ -17,13 +17,13 @@
  */
 int main() {
     // Compute the volume of a sphere of a given radius
-    const double PI = 3.14159;
+    const double PI{3.14159};
     double radius;
 
     std::cout << "Enter the radius of the sphere: ";
     std::cin >> radius;
 
-    double volume = 4 * PI * radius * radius * radius / 3;
+    double volume{4 * PI * radius * radius * radius / 3};
     std::cout << "The volume of a sphere of radius "
               << radius << " inches is " << volume
               << " cubic inches.\n";
